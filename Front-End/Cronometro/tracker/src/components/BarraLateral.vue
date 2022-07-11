9e844c0e6b5d93d85e969efc112a2e183773d080
Bianca Delbage <bnckr@outlook.com>
2022-07-11 13:06:50 -0300

    uploading...

---
+++ Front-End/Cronometro/tracker/src/components/BarraLateral.vue
@@ -0,0 +1,69 @@
+<template>
+    <header>
+        <h1>
+            <img class="img_logo" src="../assets/logo.png" alt="">
+        </h1>
+        <button class="button botao" @click="alterarTema">
+            {{textoBotao}}
+        </button>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        name: 'BarraLateral',
+        emits: ['aoTemaAlterado'],
+        data () {
+            return {
+                modoEscuroAtivo: false
+            }
+        },
+        computed: {
+            textoBotao () {
+                if(this.modoEscuroAtivo) {
+                    return `Modo Normal`
+                }
+                return `Modo Escuro`
+            }
+        },
+        methods: {
+            alterarTema () {
+                this.modoEscuroAtivo = !this.modoEscuroAtivo
+                this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+            }
+        }
+    })
+</script>
+
+<style scoped>
+header {
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+.img_logo {
+    width: 90px;
+    margin-top: 5%;
+}
+
+.botao {
+    margin: 10%;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+
+    .img_logo {
+    width: 100px;
+    align-self: center;
+}
+}
+
+</style>
---
+++ Front-End/Cronometro/tracker/src/components/Formulario.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="box formulario">
+        <div class="columns">
+            <div class="column is-8" role="form" area-label="Formulário">
+                <input type="text" class="input" placeholder="Digite a tarefa" v-model="descricao">
+            </div>
+            <div class="column">
+                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from './Temporizador.vue'
+
+export default defineComponent({
+    name: '_formulario',
+    emits: ['aoSalvarTarefa'],
+    components: {
+        Temporizador
+    },
+    data() {
+        return {
+            descricao: ''
+        }
+    },
+    methods: {
+        finalizarTarefa(tempoDecorrido: number): void {
+            this.$emit('aoSalvarTarefa', {
+                duracaoEmSegundos: tempoDecorrido,
+                descricao: this.descricao
+            })
+            this.descricao = ''
+        }
+    }
+});
+</script>
+
+<style>
+.formulario {
+    background-color: var(--bg-primario);
+    color: var(--texto-primario);
+}
+</style>
